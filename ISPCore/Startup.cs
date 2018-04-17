5b7eaa65673144d0026f46673481669f1dfd3326
cores-system <dev@sped-mail.ru>
2018-04-17 11:25:03 +0300

    Update .Net Core

--- ISPCore/Startup.cs
+++ ISPCore/Startup.cs
@@ -48,7 +48,7 @@ namespace ISPCore
         public static LatestVersion version => new LatestVersion()
         {
             Version = 2.1,
-            Patch = 7
+            Patch = 8
         };
 
         public static LatestVersion vSql => new LatestVersion()
