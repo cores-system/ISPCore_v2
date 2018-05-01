759a27c2e8e69f604f1a09bb85c6ba1f9d03dad9
cores-system <dev@sped-mail.ru>
2018-05-01 10:28:38 +0300

    Добавлена навигация в белом списке

--- ISPCore/Controllers/Settings/WhiteList/Systems.cs
+++ ISPCore/Controllers/Settings/WhiteList/Systems.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
 using ISPCore.Engine.Base;
 using ISPCore.Models.Response;
 using ISPCore.Models.Databases;
 using Microsoft.EntityFrameworkCore;
 using ISPCore.Engine.Base.SqlAndCache;
 using ISPCore.Models.Base;
+using ISPCore.Engine.Common.Views;
 
 namespace ISPCore.Controllers
 {
     public class SettingsToSystemWhiteList : ControllerToDB
     {
         [HttpGet]
-        public IActionResult Index(bool ajax)
+        public IActionResult Index(bool ajax, int page = 1)
         {
-            ViewData["ajax"] = ajax;
-            return View("/Views/Settings/WhiteList/Systems.cshtml", coreDB.WhitePtrIPs.AsNoTracking().AsEnumerable().Reverse());
+            var navPage = new NavPage<WhitePtrIP>(coreDB.WhitePtrIPs.AsNoTracking(), HttpContext, 20, page);
+            return View("~/Views/Settings/WhiteList/Systems.cshtml", navPage, ajax);
         }
 
         #region Remove
